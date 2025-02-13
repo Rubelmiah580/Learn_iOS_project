6c5fd6b45a9143efde1363292a192592b786a7c6
Rubel Miah <rublemiah2005@gmail.com>
2025-02-13 19:14:28 +0600

    Add files via upload

---
+++ multiple page/multiple page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  multiple page
+//
+//  Created by MacBook Pro on 12/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
